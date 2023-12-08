3545216e32a69e4844498d0c1fd1636328135a24
kylo <85110815+kylokims@users.noreply.github.com>
2023-12-08 21:53:54 +0800

    Update 2023_12_07_161436_create_products_table.php

--- database/migrations/2023_12_07_161436_create_products_table.php
+++ database/migrations/2023_12_07_161436_create_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->increments('product_ID');
+            $table->increments('id');
             $table->string('product_name');
             $table->string('product_desc');
             $table->decimal('price', 10, 2);
