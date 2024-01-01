2081f5eb6f6a99a385ef657152430f5658231eee
salehuddn <muhammadsalehuddin1@gmail.com>
2024-01-01 23:42:53 +0800

    feat: add index brand (admin)

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class BrandController extends Controller
 {
-    //
+    public function index ()
+    {
+        $brands = Brand::all();
+        return view('brands.index', compact('brands'));
+    }
 }
