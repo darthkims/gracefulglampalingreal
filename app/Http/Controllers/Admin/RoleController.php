6af90478069c91639ab3a1b8a2d978056ae5618b
salehuddn <muhammadsalehuddin1@gmail.com>
2024-01-02 22:01:42 +0800

    feat: add roles index, create, edit

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -3,9 +3,64 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redirect;
 
 class RoleController extends Controller
 {
-    //
+    public function index ()
+    {
+        $roles = Role::orderBy('created_at', 'desc')->get();
+
+        return view('admin.role.index', compact('roles'));
+    }
+
+    public function create()
+    {
+        return view('admin.role.create');
+    }
+
+    public function store(Request $request)
+    {
+        //check for name uniqe
+        $request->validate([
+            'name' => 'required|unique:roles',
+        ]);
+
+        Role::create([
+            'name' => $request->name,
+        ]);
+
+        return Redirect::route('roles.index')->with('message', 'Role has been created');
+    }
+
+    public function edit($id)
+    {
+        $role = Role::findOrFail($id);
+
+        return view('admin.role.edit', compact('role'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        //check for name uniqe
+        $request->validate([
+            'name' => 'required|unique:roles',
+        ]);
+
+        Role::where('id', $id)->update([
+            'name' => $request->name,
+        ]);
+
+        return Redirect::route('roles.index')->with('message', 'Role has been updated');
+    }
+
+    public function destroy($id)
+    {
+        // Student::where('student_id', $id)->delete();
+        Role::destroy($id);
+
+        return Redirect::route('roles.index')->with('message', 'Role has been deleted');
+    }
 }
---
+++ resources/views/admin/role/create.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Create Role') }}</div>
+
+                <div class="card-body">
+                    <div class="container mb-4">
+                      <div class="row">
+                        <div class="col">
+                          <a href="{{ route('roles.index') }}" class="link-dark text-decoration-none">
+                            <i class="fa-solid fa-arrow-left"></i>
+                          </a>
+                        </div>
+                        <div class="col"></div>
+                        <div class="col"></div>
+                    </div>
+
+                    <form class="my-4"
+                      action="{{ route('roles.store') }}"
+                      method="POST">
+                      @csrf
+
+                      <div class="mb-3">
+                        <label for="name" class="form-label">
+                          Role Name <span class="text-danger">*</span>
+                        </label>
+                        <input type="name" class="form-control  @error('name') is-invalid @enderror" name="name">
+
+                          @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                          @enderror
+                      </div>
+                      
+                      <div class="d-flex justify-content-end">
+                        <button type="submit" class="btn btn-success">Submit</button>
+                      </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
