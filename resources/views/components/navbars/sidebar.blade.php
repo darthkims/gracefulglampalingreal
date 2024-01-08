37c8c29ae0bbc351bae2b3cdd9b3ec5723887ce2
darthkims <85110815+kylokims@users.noreply.github.com>
2024-01-09 00:20:48 +0800

    Update sidebar.blade.php

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -44,7 +44,7 @@
             @role('admin')
                 <li class="nav-item">
                     <a class="nav-link text-white {{ $activePage == 'users' ? ' active bg-gradient-primary' : '' }} "
-                        href="{{ route('dashboard') }}">
+                        href="{{ route('users.index') }}">
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="material-icons opacity-10">group</i>
                         </div>
