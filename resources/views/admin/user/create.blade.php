24acbaccd3fac5a59cae1a66bfe5573494002526
darthkims <85110815+kylokims@users.noreply.github.com>
2024-01-09 00:20:57 +0800

    Update create.blade.php

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -1,122 +1,120 @@
-@extends('layouts.app')
+<x-layout bodyClass="g-sidenav-show bg-gray-200">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Create User') }}</div>
+    <x-navbars.sidebar activePage="user-management"></x-navbars.sidebar>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+        <!-- Navbar -->
+        <x-navbars.navs.auth titlePage="User Management"></x-navbars.navs.auth>
+        <!-- End Navbar -->
+        <div class="container-fluid py-4">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card my-4">
+                        <div class="card-body px-4 pb-2">
+                            <form class="my-4" action="{{ route('users.store') }}" method="POST">
+                                @csrf
 
-                <div class="card-body">
-                    <div class="container mb-4">
-                      <div class="row">
-                        <div class="col">
-                          <a href="{{ route('users.index') }}" class="link-dark text-decoration-none">
-                            <i class="fa-solid fa-arrow-left"></i>
-                          </a>
+                                <div class="mb-3">
+                                    <label for="name" class="form-label">
+                                        Name <span class="text-danger">*</span>
+                                    </label>
+                                    <div class="input-group input-group-outline">
+                                     <input type="text" class="form-control @error('name') is-invalid @enderror" name="name">
+                                    </div>
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="username" class="form-label">
+                                        Username <span class="text-danger">*</span>
+                                    </label>
+                                    <div class="input-group input-group-outline">
+                                      <input type="text" class="form-control @error('username') is-invalid @enderror" name="username">
+                                    </div>
+                                    @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">
+                                        Email <span class="text-danger">*</span>
+                                    </label>
+                                    <div class="input-group input-group-outline">
+                                      <input type="email" class="form-control @error('email') is-invalid @enderror" name="email">
+                                    </div>
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">
+                                        Password <span class="text-danger">*</span>
+                                    </label>
+                                    <div class="input-group input-group-outline">
+                                      <input type="password" class="form-control @error('password') is-invalid @enderror" name="password">
+                                    </div>
+
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="phone" class="form-label">
+                                        Phone No.
+                                    </label>
+                                    <div class="input-group input-group-outline">
+                                        <input type="text" class="form-control @error('phone') is-invalid @enderror" name="phone">
+                                    </div>
+
+                                    @error('phone')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="role" class="form-label">
+                                        Role <span class="text-danger">*</span>
+                                    </label>
+
+                                    @foreach ($roles as $role)
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="role" id="{{ $role->name }}" value="{{ $role->name }}">
+                                        <label class="form-check-label" for="{{ $role->name }}">{{ Str::ucfirst($role->name) }}</label>
+                                    </div>
+                                    @endforeach
+
+                                    @error('role')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <div class="d-flex justify-content-end">
+                                    <button type="submit" class="btn btn-success">Submit</button>
+                                </div>
+                            </form>
                         </div>
-                        <div class="col"></div>
-                        <div class="col"></div>
                     </div>
-
-                    <form class="my-4"
-                      action="{{ route('users.store') }}"
-                      method="POST">
-                      @csrf
-
-                      <div class="mb-3">
-                        <label for="name" class="form-label">
-                          Name <span class="text-danger">*</span>
-                        </label>
-                        <input type="text" class="form-control  @error('name') is-invalid @enderror" name="name">
-
-                          @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                          @enderror
-                      </div>
-
-                      <div class="mb-3">
-                        <label for="username" class="form-label">
-                          Username <span class="text-danger">*</span>
-                        </label>
-                        <input type="text" class="form-control  @error('username') is-invalid @enderror" name="username">
-
-                          @error('username')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                          @enderror
-                      </div>
-
-                      <div class="mb-3">
-                        <label for="email" class="form-label">
-                          Email <span class="text-danger">*</span>
-                        </label>
-                        <input type="email" class="form-control  @error('email') is-invalid @enderror" name="email">
-
-                          @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                          @enderror
-                      </div>
-
-                      <div class="mb-3">
-                        <label for="role" class="form-label">
-                          Password <span class="text-danger">*</span>
-                        </label>
-                        <br>
-                        <input type="password" class="form-control  @error('password') is-invalid @enderror" name="password" >
-
-                        @error('password')
-                          <span class="invalid-feedback" role="alert">
-                              <strong>{{ $message }}</strong>
-                          </span>
-                        @enderror
-                      </div>
-
-                      <div class="mb-3">
-                        <label for="phone" class="form-label">
-                          Phone No.
-                        </label>
-                        <input type="text" class="form-control  @error('phone') is-invalid @enderror" name="phone">
-
-                          @error('phone')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                          @enderror
-                      </div>
-
-                      <div class="mb-3">
-                        <label for="role" class="form-label">
-                          Role <span class="text-danger">*</span>
-                        </label>
-                        <br>
-
-                        @foreach ($roles as $role)
-                          <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="role" id="{{ $role->name }}" value="{{ $role->name }}">
-                            <label class="form-check-label" for="{{ $role->name }}">{{ Str::ucfirst($role->name) }}</label>
-                          </div>
-                        @endforeach
-
-                          @error('role')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                          @enderror
-                      </div>
-
-                      <div class="d-flex justify-content-end">
-                        <button type="submit" class="btn btn-success">Submit</button>
-                      </div>
-                    </form>
                 </div>
             </div>
+            <x-footers.auth></x-footers.auth>
         </div>
-    </div>
-</div>
-@endsection
+    </main>
+    <x-plugins></x-plugins>
+
+</x-layout>
