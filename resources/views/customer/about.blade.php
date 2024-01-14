7780f4a03a6f660b5589d4192bf2fafc91ee938c
HaziqAimanFauzi <136706089+HaziqAimanFauzi@users.noreply.github.com>
2024-01-14 15:35:17 +0800

    Feat: about add gambar hihi

--- resources/views/customer/about.blade.php
+++ resources/views/customer/about.blade.php
@@ -38,22 +38,19 @@
                 <div class="col-lg-4 col-md-4 col-sm-6">
                     <div class="about__item">
                         <h4>Who We Are ?</h4>
-                        <p>Contextual advertising programs sometimes have strict policies that need to be adhered too.
-                        Let’s take Google as an example.</p>
+                        <p>GracefulGlamp is your go-to personal shopper website, dedicated to providing a seamless and personalized shopping experience. Our platform features a curated selection of the latest trends in fashion, beauty, and lifestyle. </p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-6">
                     <div class="about__item">
                         <h4>Who We Do ?</h4>
-                        <p>In this digital generation where information can be easily obtained within seconds, business
-                        cards still have retained their importance.</p>
+                        <p>With expert personal shoppers on hand, we offer tailored recommendations based on individual tastes. Our user-friendly interface, commitment to quality, and secure transactions ensure a worry-free and enjoyable shopping journey. </p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-6">
                     <div class="about__item">
                         <h4>Why Choose Us</h4>
-                        <p>A two or three storey house is the ideal way to maximise the piece of earth on which our home
-                        sits, but for older or infirm people.</p>
+                        <p>Quality is our priority. Each item featured on GracefulGlamp meets our high standards of excellence. Our user-friendly interface makes shopping enjoyable and efficient, and you can trust our secure platform for worry-free transactions.</p>
                     </div>
                 </div>
             </div>
@@ -125,28 +122,28 @@
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="team__item">
-                        <img src="{{ asset('customer')}}/img/about/team-1.jpg" alt="">
+                        <img src="{{ asset('customer')}}/img/about/kims.jpeg" alt="">
                         <h4>Salehuddin</h4>
                         <span>Founder</span>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="team__item">
-                        <img src="{{ asset('customer')}}/img/about/team-2.jpg" alt="">
+                        <img src="{{ asset('customer')}}/img/about/kims.jpeg" alt="">
                         <h4>Hakimi</h4>
                         <span>C.E.O</span>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="team__item">
-                        <img src="{{ asset('customer')}}/img/about/team-3.jpg" alt="">
+                        <img src="{{ asset('customer')}}/img/about/kims.jpeg" alt="">
                         <h4>Haziq</h4>
                         <span>Manager</span>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="team__item">
-                        <img src="{{ asset('customer')}}/img/about/team-4.jpg" alt="">
+                        <img src="{{ asset('customer')}}/img/about/kims.jpeg" alt="">
                         <h4>Arief</h4>
                         <span>Supervisor</span>
                     </div>
@@ -169,28 +166,28 @@
             </div>
             <div class="row">
                 <div class="col-lg-3 col-md-4 col-sm-4 col-6">
-                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/client-1.png" alt=""></a>
+                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/nike.png" alt=""></a>
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-4 col-6">
-                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/client-2.png" alt=""></a>
+                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/h&m.png" alt=""></a>
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-4 col-6">
-                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/client-3.png" alt=""></a>
+                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/yankees.png" alt=""></a>
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-4 col-6">
-                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/client-4.png" alt=""></a>
+                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/levi.png" alt=""></a>
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-4 col-6">
-                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/client-5.png" alt=""></a>
+                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/outlet.png" alt=""></a>
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-4 col-6">
-                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/client-6.png" alt=""></a>
+                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/lauder.png" alt=""></a>
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-4 col-6">
-                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/client-7.png" alt=""></a>
+                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/vans.png" alt=""></a>
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-4 col-6">
-                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/client-8.png" alt=""></a>
+                    <a href="#" class="client__item"><img src="{{ asset('customer')}}/img/clients/ioi.png" alt=""></a>
                 </div>
             </div>
         </div>
