b7cf2ab82771b8c5d8100f0499ad0132e6b6c01d
darthkims <85110815+kylokims@users.noreply.github.com>
2024-01-15 12:36:44 +0800

    Update sidebar.blade.php

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -54,7 +54,7 @@
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="material-icons opacity-10">shopping_cart</i>
                         </div>
-                        <span class="nav-link-text ms-1">Cart</span>
+                        <span class="nav-link-text ms-1">Orders</span>
                     </a>
                 </li>
                 <li class="nav-item">
