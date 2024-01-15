cd35cf21e434b732be5dfa4b1818ba48905eeb90
salehuddn <muhammadsalehuddin1@gmail.com>
2024-01-15 20:34:04 +0800

    feat: clear cart after checkout

---
+++ database/migrations/2024_01_15_184701_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->decimal('total', 10, 2)->default(0.00);
+            $table->string('status')->default('PENDING PAYMENT');
+            $table->timestamps();
+        });
+        
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_product');
+        Schema::dropIfExists('orders');
+    }
+};
