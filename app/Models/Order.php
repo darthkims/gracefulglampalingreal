98ad00548586084e16a8327c8be378ed9d74de91
salehuddn <muhammadsalehuddin1@gmail.com>
2024-01-15 22:41:47 +0800

    feat: add order number

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,6 +10,7 @@ class Order extends Model
     use HasFactory;
 
     protected $fillable = [
+        'order_number',
         'user_id', 
         'promo_code_id',
         'total',
@@ -31,5 +32,37 @@ class Order extends Model
         return $this->belongsToMany(Product::class, 'order_product')
                     ->withPivot('quantity')
                     ->withTimestamps();
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // When creating a new order, automatically set the order_number
+        static::creating(function ($order) {
+            $order->order_number = 'GG' . now()->format('Ym') . static::generateIncrementNumber();
+        });
+    }
+
+    // Generate an increment number based on the current month
+    protected static function generateIncrementNumber()
+    {
+        $lastOrder = static::orderBy('id', 'desc')->first();
+
+        if ($lastOrder) {
+            $lastOrderNumber = $lastOrder->order_number;
+
+            // Extract the numeric part and increment it
+            $numericPart = (int) substr($lastOrderNumber, -3);
+            $newNumericPart = $numericPart + 1;
+
+            // Pad the new numeric part with zeros to ensure it has three digits
+            $newNumericPart = str_pad($newNumericPart, 3, '0', STR_PAD_LEFT);
+
+            return $newNumericPart;
+        } else {
+            // If no previous orders exist, start with 001
+            return '001';
+        }
     }
 }
--- database/migrations/2024_01_15_184701_create_orders_table.php
+++ database/migrations/2024_01_15_184701_create_orders_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('order_number')->unique();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->decimal('total', 10, 2)->default(0.00);
